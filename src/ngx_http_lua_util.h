74f95e8eac38f7ff22d4d52910b1d88a07578808
Jaroslav Barton <jaroslav@bartonovi.eu>
2020-01-28 15:41:06 +0100

    Another try on bind support

--- src/ngx_http_lua_util.h
+++ src/ngx_http_lua_util.h
@@ -254,6 +254,9 @@ void ngx_http_lua_cleanup_free(ngx_http_request_t *r,
 void ngx_http_lua_set_sa_restart(ngx_log_t *log);
 #endif
 
+ngx_addr_t *ngx_http_lua_parse_addr(lua_State *L, u_char *text, size_t len);
+
+
 #define ngx_http_lua_check_if_abortable(L, ctx)                              \
     if ((ctx)->no_abort) {                                                   \
         return luaL_error(L, "attempt to abort with pending subrequests");   \
